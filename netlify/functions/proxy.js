a6c42c0ce6079426010b99f87bae0a33b461ba6f
existenz31 <spaltrie1@gmail.com>
2024-02-06 17:02:54 +0100

    cgange browse into search

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -9,21 +9,20 @@ exports.handler = async event => {
             const ALGOLIA_APP_ID = requestBody.AppId;
             const ALGOLIA_API_KEY = requestBody.ApiKey;
             const ALGOLIA_INDEX_NAME = requestBody.IndexName;
-            // const query = requestBody.query || '';
-            // const queryParams = requestBody.queryParams || {};
+            const query = requestBody.query || '';
+            const queryParams = requestBody.queryParams || {};
+            console.log('query: ', query);
+            console.log('queryParams: ', JSON.stringify(queryParams));
 
             const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
             const index = client.initIndex(ALGOLIA_INDEX_NAME);
 
-            let hits = [];
+            //let hits = [];
 
-            await index.browseObjects({
-                batch: batch => {
-                    hits = hits.concat(batch);
-                }
-            }).then(() => console.log(hits))
+            const hits = await index.search(query, queryParams)
+                .then(({ hits }) => hits)
                 .catch(error => {
-                    console.error(error);
+                    console.error('error :', error);
                 });
 
             // Return a success response
@@ -33,6 +32,7 @@ exports.handler = async event => {
             };
         } catch (error) {
             // Return an error response if there was an issue processing the request
+            console.error('error2 :', error);
             return {
                 statusCode: 400,
                 body: JSON.stringify({ error: 'Failed to process POST request =>' + error }),
